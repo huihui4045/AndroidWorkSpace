fec32ebdcc909a3970348251f92cae75978753fb
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-02-21 18:36:49 +0800

    Revert "Android 内存泄漏"
    This reverts commit 2f079ae0501f5fe07eadb740b63cb32b79ce7379.

--- Android_Demo/app/src/main/java/com/huihui/demo/CActivity.java
+++ Android_Demo/app/src/main/java/com/huihui/demo/CActivity.java
@@ -1,49 +1,25 @@
 package com.huihui.demo;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class CActivity extends BaseActivity {
 
-    private static Context mContext;
-
-    private static TextView mTextView;
-    private Button mButton;
-    private ObjectAnimator animator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c);
-        mContext = this;
-        mTextView = new TextView(this);
-        mButton = ((Button) findViewById(R.id.btn_c));
-        mButton.setOnClickListener(new View.OnClickListener() {
+
+
+        findViewById(R.id.btn_c).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(CActivity.this, BActivity.class);
 
                 startActivity(intent);
             }
         });
-         animator=ObjectAnimator.ofFloat(mButton,"rotation",0,360).setDuration(2000);
-
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.start();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (animator!=null){
-           // animator.cancel();
-        }
     }
 }
-
--- Android_Demo/app/src/main/java/com/huihui/demo/MainActivity.java
+++ Android_Demo/app/src/main/java/com/huihui/demo/MainActivity.java
@@ -6,53 +6,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.view.ViewStub;
-import android.widget.TextView;
 
-import com.huihui.demo.activity.WebActivity;
-
-import java.lang.ref.WeakReference;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class MainActivity extends BaseActivity {
-    private CopyOnWriteArrayList<String> datas = new CopyOnWriteArrayList<>();
-    private static int value = 10;
-    private TextView mValue;
-    private Handler mHandler = new MyHandler(this);
+public class MainActivity extends BaseActivity implements Handler.Callback{
 
-    private static class MyHandler extends Handler {
-        public WeakReference<MainActivity> mainActivity;
+    private Handler handler=new Handler(this);
 
-        public MyHandler(MainActivity mainActivity) {
-            this.mainActivity = new WeakReference<>(mainActivity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            MainActivity mainActivity = this.mainActivity.get();
-            if (mainActivity != null) {
-                // TODO: 2017/2/20
-            }
-        }
-    }
-
-    /**
-     * 使用静态的内部类，不会持有当前对象的引用
-     */
-    private static final Runnable sRunnable = new Runnable() {
-        @Override
-        public void run() {
-        }
-    };
+    private CopyOnWriteArrayList<String> datas=new CopyOnWriteArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mHandler.postDelayed(sRunnable, 600000);
-
 
         findViewById(R.id.btn_a).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,39 +29,21 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-        findViewById(R.id.btn_web).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WebActivity.class));
-            }
-        });
-
-        mValue = ((TextView) findViewById(R.id.tv_value));
-        mValue.setText(String.valueOf(value));
-
-        findViewById(R.id.update).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value = 100;
-
-                mValue.setText(String.valueOf(value));
-            }
-        });
-
 
         //handler.sendEmptyMessage(11);
 
-        Uri uri = Uri.parse("content://com.huihui.demo.BookProvider");
+        Uri uri=Uri.parse("content://com.huihui.demo.BookProvider");
 
-        getContentResolver().query(uri, null, null, null, null);
-        getContentResolver().query(uri, null, null, null, null);
-        getContentResolver().query(uri, null, null, null, null);
-
-
-        //((ViewStub) findViewById(R.id.view_stub)).setVisibility(View.VISIBLE);
-        View inflate = ((ViewStub) findViewById(R.id.view_stub)).inflate();
-
+        getContentResolver().query(uri,null,null,null,null);
+        getContentResolver().query(uri,null,null,null,null);
+        getContentResolver().query(uri,null,null,null,null);
 
     }
 
+    @Override
+    public boolean handleMessage(Message msg) {
+
+
+        return false;
+    }
 }
